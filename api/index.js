2cd4c0860986cfef545ab47b2c4d417d91d86e35
Mohamad Rkein <mohammadrkein79@gmail.com>
2024-01-26 22:48:20 +0200

    Create User Model

--- api/index.js
+++ api/index.js
@@ -1,23 +1,18 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 dotenv.config();
 
 mongoose
-    .connect(process.env.MONGO)
-    .then(() =>{
-
-console.log("Connected To MongoDB");
-    } 
-    )
-    .catch((err)=>{
-        console.log(err);
-    });
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected To MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
-app.listen(3000,()=>{ 
-
-console.log("Server is RUNNING on port 3000");
-
-}
-);
+app.listen(3000, () => {
+  console.log("Server is RUNNING on port 3000");
+});
