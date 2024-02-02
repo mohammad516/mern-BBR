c88c4866cb2ce0922bd6e601af4cf1684268d8e6
Mohamad Rkein <mohammadrkein79@gmail.com>
2024-02-02 15:38:44 +0200

    Complete sign in page functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="bg-slate-200 shadow-md">
+    <header className="bg-neutral-700 shadow-md">
       {/*bg means background */}
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to={"/"}>
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">BBR</span>
-            <span className="text-slate-700">.net</span>
+            <span className="text-cyan-500">BBR</span>
+            <span className="text-blue-100">.net</span>
           </h1>
         </Link>
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
@@ -22,17 +22,17 @@ export default function Header() {
         </form>
         <ul className="flex gap-4">
             <Link to={"/"}>
-          <li className="hidden sm:inline text-slate-700 hover:underline">
+          <li className="hidden sm:inline text-blue-100 hover:underline">
             Home
           </li>
           </Link>
           <Link to={"/about"}>
-          <li className="hidden sm:inline text-slate-700 hover:underline">
+          <li className="hidden sm:inline text-blue-100 hover:underline">
             About
           </li>
           </Link>
           <Link to={"/sign-in"}>
-          <li className=" text-slate-700 hover:underline">SignIn</li>
+          <li className=" text-blue-100 hover:underline">SignIn</li>
           </Link>
         </ul>
       </div>
